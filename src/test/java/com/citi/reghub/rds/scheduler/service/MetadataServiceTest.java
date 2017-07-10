9c3c792d14cd3a16c640be13c325942058735661
mt66710 <mt66710@imcnam.ssmb.com>
2017-07-10 17:42:18 -0400

    C170530, hotfix teamcity build failure: removed test code that uses external resources.

--- src/test/java/com/citi/reghub/rds/scheduler/service/MetadataServiceTest.java
+++ src/test/java/com/citi/reghub/rds/scheduler/service/MetadataServiceTest.java
@@ -4,40 +4,20 @@ import java.util.List;
 import java.util.Map;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
-import org.springframework.test.context.junit4.SpringRunner;
 
-import com.citi.reghub.rds.scheduler.RdsBatchConfiguration;
-import com.citi.reghub.rds.scheduler.RdsSchedulerConfiguration;
-
-@RunWith(SpringRunner.class)
-@SpringBootTest
-@Import({RdsSchedulerConfiguration.class, RdsBatchConfiguration.class})
 public class MetadataServiceTest {
-	@Autowired
 	MetadataService metadataService;
+
+	@Before
+	public void init() {
+		metadataService = new MetadataService();
+	}
 
 	@Test
 	public void testGetDatabases() {
 		Map<String, List<String>> dbs = metadataService.getDatabases();
 		Assert.assertNotNull(dbs);
-	}
-	
-	@Test
-	public void testGetDatabase() {
-		String db = metadataService.getDatabase();
-		Assert.assertNotNull("Database name is null.", db);
-		Assert.assertNotEquals("Database name is empty.", db, "");
-	}
-	
-	@Test
-	public void testGetCollection() {
-		String col = metadataService.getDatabase();
-		Assert.assertNotNull("Collection name is null.", col);
-		Assert.assertNotEquals("Collection name is empty.", col, "");
 	}
 }
