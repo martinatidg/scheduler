6a2344a6993400baa90b1901aaed0d69306ea73a
mt66710 <mt66710@imcnam.ssmb.com>
2017-07-17 17:44:12 -0400

    C170530-840, removed the workaround method for Sonar complain.

--- src/main/java/com/citi/reghub/rds/scheduler/RdsSchedulerApplication.java
+++ src/main/java/com/citi/reghub/rds/scheduler/RdsSchedulerApplication.java
@@ -10,11 +10,4 @@ public class RdsSchedulerApplication {
 	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		SpringApplication.run(RdsSchedulerApplication.class, args);
 	}
-
-	// Sonar requires to provide a private constructor for classes with only static methods.
-	// However if a private constructor is provided, the Spring Boot default test will fail.
-	// This method is a workaround to avoid the sonar complaint.
-	public void doNothing() {
-		throw new UnsupportedOperationException("Workaround to avoid Sonar complains for no private constructor.");
-	}
 }
